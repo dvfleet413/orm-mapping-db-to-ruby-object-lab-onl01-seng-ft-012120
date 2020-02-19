641c2a42eed0f9468215e6a400522444fb90017a
David Van Fleet <dvfleet413@gmail.com>
2020-02-19 22:39:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,6 +13,8 @@ class Student
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+    sql = <<-SQL
+      
   end
   
   def save
