b8ee289518a3e9fcd1d0013926e9f5af42913795
David Van Fleet <dvfleet413@gmail.com>
2020-02-19 22:47:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -22,7 +22,25 @@ class Student
     end 
   end
   
-  def 
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade = 9;
+    SQL
+    DB[:conn].execute(sql).collect do |row|
+      self.new_from_db(row)
+    end 
+  end
+  
+  def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade < 12;
+    SQL
+    DB[:conn].execute(sql).collect do |row|
+      self.new_from_db(row)
+    end 
+  end
 
   def self.find_by_name(name)
     # find the student in the database given a name
